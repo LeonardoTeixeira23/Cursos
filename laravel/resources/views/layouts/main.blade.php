dc72987c5d735572bb4107090e2bc4150885e4f2
leonardoteixeira23 <leo.dst23@gmail.com>
2022-05-27 20:16:14 -0300

    .

---
+++ laravel/resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+@extends('main');
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<header>
+    <h1>Test</h1>
+</header>
+
+<body>
+    
+</body>
+
+<footer>
+    <p>Test</p>
+</footer>
+</html>
